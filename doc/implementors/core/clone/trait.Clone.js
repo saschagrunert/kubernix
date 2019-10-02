2bc89b5660083239c2f8938c0e71a98610bd8624
CircleCI <mail@saschagrunert.de>
2019-10-02 18:55:41 +0000

    Update documentation

--- doc/implementors/core/clone/trait.Clone.js
+++ doc/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",synthetic:false,types:["kubernix::config::Config"]},];
+implementors["kubernix"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"kubernix/struct.ConfigBuilder.html\" title=\"struct kubernix::ConfigBuilder\">ConfigBuilder</a>",synthetic:false,types:["kubernix::config::ConfigBuilder"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
