c5c1a0c6e571c6e26d1e23fa7df1cc83442a74ca
Sascha Grunert <sgrunert@suse.com>
2019-10-05 09:13:39 +0200

    Update and rename cargo-kcov.nix to grcov.nix

---
+++ nix/grcov.nix
@@ -0,0 +1,18 @@
+{ lib, fetchurl, mkDerivation }:
+
+mkDerivation rec {
+  name = "grcov";
+  version = "0.5.4";
+
+  src = fetchurl {
+    url = "https://github.com/mozilla/grcov/releases/download/v0.5.4/grcov-linux-x86_64.tar.bz2";
+    sha256 = "0hqplgj3i8js42v2kj44khk543a93sk3n6wlfpv3c84pdqlm29br";
+  };
+
+  buildPhase = "";
+  installPhase = ''
+    mkdir -p $out/bin
+    ls -lah
+    cp foo $out/bin
+  '';
+}
