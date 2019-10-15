aa93ad575ea84cfa0062b0e83e38201f9142d2e9
Sascha Grunert <sgrunert@suse.com>
2019-10-15 20:50:04 +0200

    Update integration.rs

--- tests/integration.rs
+++ tests/integration.rs
@@ -8,7 +8,7 @@ use std::{
     time::Instant,
 };
 
-const TIMEOUT: u64 = 300;
+const TIMEOUT: u64 = 600;
 const PATTERN: &str = "Spawning interactive shell";
 
 fn test(run: usize, args: &[&str]) -> Fallible<()> {
