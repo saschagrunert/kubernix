f63b82d96dad260c2e4807279c5bfb934555432a
Sascha Grunert <sgrunert@suse.com>
2019-10-19 21:54:04 +0200

    Update e2e.rs

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -4,20 +4,35 @@ use common::{run_local_test, run_root, SUDO};
 use failure::{bail, Fallible};
 use std::{env::var, process::Command};
 
+// Possible conformance test scopes for now:
+// [k8s.io]
+// [sig-api-machinery]
+// [sig-apps]
+// [sig-auth]
+// [sig-cli]
+// [sig-network]
+// [sig-node]
+// [sig-scheduling]
+// [sig-storage]
+
 #[test]
-fn local_single_node() -> Fallible<()> {
-    let test = "e2e-single-node";
+fn local_single_node_sig_cli() -> Fallible<()> {
+    e2e_local_single_node("sig-cli")
+}
+
+fn e2e_local_single_node(focus: &str) -> Fallible<()> {
+    let test = &format!("e2e-{}", focus);
     run_local_test(test, None, || {
         let kubeconfig = run_root(test).join("kubeconfig").join("admin.kubeconfig");
         if !Command::new(SUDO)
-	    .arg("env")
+            .arg("env")
             .arg(format!("PATH={}", var("PATH")?))
             .arg(format!("KUBECONFIG={}", kubeconfig.display()))
             .arg("KUBERNETES_SERVICE_HOST=127.0.0.1")
             .arg("KUBERNETES_SERVICE_PORT=6443")
             .arg("e2e.test")
             .arg("--provider=local")
-            .arg("--ginkgo.focus=.*\\[Conformance\\].*")
+            .arg(format!("--ginkgo.focus=.*\\[{}\\].*", focus))
             .status()?
             .success()
         {
