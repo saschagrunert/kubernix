bc99e096899b329a08e7717dde6a54d19574c9ab
Sascha Grunert <sgrunert@suse.com>
2019-10-19 21:05:40 +0200

    Update e2e.rs

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -2,7 +2,7 @@ mod common;
 
 use common::{run_local_test, run_root, SUDO};
 use failure::{bail, Fallible};
-use std::{env::var, net::Ipv4Addr, process::Command};
+use std::{env::var, process::Command};
 
 #[test]
 fn local_single_node() -> Fallible<()> {
@@ -12,14 +12,14 @@ fn local_single_node() -> Fallible<()> {
         if !Command::new(SUDO)
 	    .arg("env")
             .arg(format!("PATH={}", var("PATH")?))
+            .arg(format!("KUBECONFIG={}", kubeconfig.display())
+            .arg("KUBERNETES_SERVICE_HOST=127.0.0.1")
+            .arg("KUBERNETES_SERVICE_PORT=6443")
             .arg("e2e.test")
             .arg("--provider=local")
             // TODO: enable more tests
             // .arg("--ginkgo.focus=.*\\[Conformance\\].*")
             .arg("--ginkgo.focus=.*should serve a basic endpoint from pods.*")
-            .env("KUBECONFIG", kubeconfig)
-            .env("KUBERNETES_SERVICE_HOST", Ipv4Addr::LOCALHOST.to_string())
-            .env("KUBERNETES_SERVICE_PORT", "6443")
             .status()?
             .success()
         {
