abe84cdeec5b683d27572619144ab05443343518
Sascha Grunert <sgrunert@suse.com>
2019-10-19 18:28:19 +0200

    Fix env var PATH reset

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -2,22 +2,22 @@ mod common;
 
 use common::{find_executable, run_local_test, run_root, SUDO};
 use failure::{bail, Fallible};
-use std::{net::Ipv4Addr, process::Command};
+use std::{env::var, net::Ipv4Addr, process::Command};
 
 #[test]
 fn local_single_node() -> Fallible<()> {
     let test = "e2e-single-node";
     run_local_test(test, None, || {
         let kubeconfig = run_root(test).join("kubeconfig").join("admin.kubeconfig");
-        let e2e = find_executable("e2e.test")?;
         if !Command::new(SUDO)
             .arg("-E")
-            .arg(e2e)
+            .arg("e2e.test")
             .arg("--provider=local")
             // TODO: enable more tests
             // .arg("--ginkgo.focus=.*\\[Conformance\\].*")
             .arg("--ginkgo.focus=.*should serve a basic endpoint from pods.*")
             .env("KUBECONFIG", kubeconfig)
+            .env("PATH", var("PATH")?)
             .env("KUBERNETES_SERVICE_HOST", Ipv4Addr::LOCALHOST.to_string())
             .env("KUBERNETES_SERVICE_PORT", "6443")
             .status()?
