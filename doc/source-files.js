bc543afafb5a3390b01e1df7347f195006332e81
CircleCI <mail@saschagrunert.de>
2019-11-01 14:58:38 +0000

    Update documentation

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["kubernix"] = {"name":"","files":["apiserver.rs","config.rs","container.rs","controllermanager.rs","coredns.rs","crio.rs","encryptionconfig.rs","etcd.rs","kubeconfig.rs","kubectl.rs","kubelet.rs","lib.rs","logger.rs","network.rs","nix.rs","node.rs","pki.rs","podman.rs","process.rs","proxy.rs","scheduler.rs","system.rs"]};
+sourcesIndex["kubernix"] = {"name":"","files":["apiserver.rs","config.rs","container.rs","controllermanager.rs","coredns.rs","crio.rs","encryptionconfig.rs","etcd.rs","kubeconfig.rs","kubectl.rs","kubelet.rs","lib.rs","logger.rs","network.rs","nix.rs","node.rs","pki.rs","podman.rs","process.rs","progress.rs","proxy.rs","scheduler.rs","system.rs"]};
 createSourceSidebar();
