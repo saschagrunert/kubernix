502af3a4fe9fbdb4f20d7bde4789de49a498ded0
CircleCI <mail@saschagrunert.de>
2019-12-16 08:36:57 +0000

    Update documentation

--- doc/implementors/log/trait.Log.js
+++ doc/implementors/log/trait.Log.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.8/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"kubernix/struct.Logger.html\" title=\"struct kubernix::Logger\">Logger</a>",synthetic:false,types:["kubernix::logger::Logger"]},];
+implementors["kubernix"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.10/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"kubernix/struct.Logger.html\" title=\"struct kubernix::Logger\">Logger</a>",synthetic:false,types:["kubernix::logger::Logger"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
