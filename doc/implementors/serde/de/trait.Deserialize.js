064d25bfab9be3e739e0f60432f6c027184675a4
CircleCI <mail@saschagrunert.de>
2019-12-16 05:34:40 +0000

    Update documentation

--- doc/implementors/serde/de/trait.Deserialize.js
+++ doc/implementors/serde/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.103/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",synthetic:false,types:["kubernix::config::Config"]},];
+implementors["kubernix"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",synthetic:false,types:["kubernix::config::Config"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
