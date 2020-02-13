db069810266aca12d3ca7ed9d3cd63069e62ad2b
CircleCI <mail@saschagrunert.de>
2020-02-13 13:16:38 +0000

    Update documentation

---
+++ doc/implementors/clap/derive/trait.IntoApp.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.0.0-beta.1/clap/derive/trait.IntoApp.html\" title=\"trait clap::derive::IntoApp\">IntoApp</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
--- doc/implementors/clap/trait.IntoApp.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.0.0-beta.1/clap/trait.IntoApp.html\" title=\"trait clap::IntoApp\">IntoApp</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
