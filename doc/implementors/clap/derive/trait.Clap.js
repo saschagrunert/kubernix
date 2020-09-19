081e15c9ccb441dcdda8a4fb78e6f2606e38d4f2
CircleCI <mail@saschagrunert.de>
2020-09-19 23:17:37 +0000

    Update documentation

--- doc/implementors/clap/derive/trait.Clap.js
+++ doc/implementors/clap/derive/trait.Clap.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.0.0-beta.1/clap/derive/trait.Clap.html\" title=\"trait clap::derive::Clap\">Clap</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
+implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.0.0-beta.2/clap/derive/trait.Clap.html\" title=\"trait clap::derive::Clap\">Clap</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
