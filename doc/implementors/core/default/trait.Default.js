75acace98aa8e9664cbc89c21ff6edb225d3c7b8
CircleCI <mail@saschagrunert.de>
2020-10-08 22:18:33 +0000

    Update documentation

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
+implementors["kubernix"] = [{"text":"impl Default for Config","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/log/trait.Log.js
+++ doc/implementors/log/trait.Log.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.11/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"kubernix/struct.Logger.html\" title=\"struct kubernix::Logger\">Logger</a>","synthetic":false,"types":["kubernix::logger::Logger"]}];
+implementors["kubernix"] = [{"text":"impl Log for Logger","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
