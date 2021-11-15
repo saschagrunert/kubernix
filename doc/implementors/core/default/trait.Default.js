7d08a6cd8a1ee969feefe1f62bab940ccfb9d447
CircleCI <mail@saschagrunert.de>
2021-11-15 15:54:18 +0000

    Update documentation

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
+implementors["kubernix"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>","synthetic":false,"types":["kubernix::config::Config"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
