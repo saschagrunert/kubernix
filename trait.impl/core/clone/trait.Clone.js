2086d7cfcb232871b90206057edc7c8fdc3fa188
saschagrunert <saschagrunert@users.noreply.github.com>
2026-03-30 12:54:06 +0000

    deploy: cf9ab8e00c8664d0d7f50acf72e637df39a6598e

--- trait.impl/core/clone/trait.Clone.js
+++ trait.impl/core/clone/trait.Clone.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[268]}
+//{"start":59,"fragment_lengths":[519]}
