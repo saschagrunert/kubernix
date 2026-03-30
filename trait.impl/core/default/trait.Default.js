5d8ed4712d89b2f22a573c69522f8d19572c02fa
saschagrunert <saschagrunert@users.noreply.github.com>
2026-03-30 09:39:22 +0000

    deploy: 04fd338b4f56a36bc3c0131bc3cfab941d9c0e06

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"kubernix/struct.Kubernix.html\" title=\"struct kubernix::Kubernix\">Kubernix</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"kubernix/struct.Kubernix.html\" title=\"struct kubernix::Kubernix\">Kubernix</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
