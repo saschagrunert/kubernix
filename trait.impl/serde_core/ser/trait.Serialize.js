f9a09188a497a93e0668934984c81e99706393af
saschagrunert <saschagrunert@users.noreply.github.com>
2026-07-20 12:12:09 +0000

    deploy: 73a687811a12fe5a94f872d7baea53c70d477595

--- trait.impl/serde_core/ser/trait.Serialize.js
+++ trait.impl/serde_core/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"kubernix/struct.Config.html\" title=\"struct kubernix::Config\">Config</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
