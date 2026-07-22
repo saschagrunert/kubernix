1de56bcc87a9905b8d3c4c583a5e14ebc312a742
saschagrunert <saschagrunert@users.noreply.github.com>
2026-07-22 10:46:19 +0000

    deploy: 0a0dc89445f26e2c34a3a80091beca6fdf51ed9a

--- trait.impl/clap_builder/derive/trait.ValueEnum.js
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl ValueEnum for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl ValueEnum for <a class=\"enum\" href=\"kubernix/enum.CriRuntime.html\" title=\"enum kubernix::CriRuntime\">CriRuntime</a>",0],["impl ValueEnum for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0]]],["kubernix",[["impl ValueEnum for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[144]}
+//{"start":59,"fragment_lengths":[277,145]}
