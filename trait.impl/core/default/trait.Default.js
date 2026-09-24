9fd7a0d9feb059fef5f4c939ce9bf2f418f7924e
saschagrunert <saschagrunert@users.noreply.github.com>
2026-09-24 09:35:44 +0000

    deploy: 71c687f3c579f722849dd064827dc73f25805627

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"kubernix/enum.CriRuntime.html\" title=\"enum kubernix::CriRuntime\">CriRuntime</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0]]]]);
+    const implementors = Object.fromEntries([["kubernix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"kubernix/enum.CriRuntime.html\" title=\"enum kubernix::CriRuntime\">CriRuntime</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"kubernix/enum.LogFormat.html\" title=\"enum kubernix::LogFormat\">LogFormat</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"kubernix/enum.OciRuntime.html\" title=\"enum kubernix::OciRuntime\">OciRuntime</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[545]}
+//{"start":59,"fragment_lengths":[812]}
